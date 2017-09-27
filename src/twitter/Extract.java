f95e77ef1e3c0476e933de76a6262d656d640452
Nick White <nawhi3@outlook.com>
2017-09-27 12:11:37 +0100

    Implement getTimespan()

--- src/twitter/Extract.java
+++ src/twitter/Extract.java
@@ -4,6 +4,8 @@
 package twitter;
 
 import java.util.List;
+import java.util.ArrayList;
+import java.time.Instant;
 import java.util.Set;
 
 /**
@@ -23,8 +25,35 @@ public class Extract {
      * @return a minimum-length time interval that contains the timestamp of
      *         every tweet in the list.
      */
-    public static Timespan getTimespan(List<Tweet> tweets) {
-        throw new RuntimeException("not implemented");
+	public static Timespan getTimespan(List<Tweet> tweets) {
+        Instant min = tweets.get(0).getTimestamp();
+        Instant max = tweets.get(0).getTimestamp();
+        return getTimespanRec(tweets, min, max);
+    }
+    
+	/**
+	 * Recursive utility method for finding the time period
+	 * spanned by tweets.
+	 * 
+	 * @param tweets 
+	 * 			list of tweets with distinct ids, not modified
+	 * @param min
+	 * 			Instant object which keeps track of the minimum time
+	 * @param max
+	 * 			Instant object which keeps track of the maximum time
+	 * @return a minimum-length time interval that contains the timestamp of
+	 * 		every tweet in the list
+	 */
+    private static Timespan getTimespanRec(final List<Tweet> tweets, Instant min, Instant max) {
+    	if (tweets.isEmpty())
+    		return new Timespan(min, max);
+    	Tweet t = tweets.get(0);
+    	if (min.compareTo(t.getTimestamp()) >= 0)
+    		min = t.getTimestamp();
+    	if (max.compareTo(t.getTimestamp()) <= 0)
+    		max = t.getTimestamp();
+    	return getTimespanRec(tweets.subList(1, tweets.size()), min, max);
+    			
     }
 
     /**
