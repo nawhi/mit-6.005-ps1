ae82a8e83ae087ca6bce0e1e75b8d2ad33205fb5
Nick White <nawhi3@outlook.com>
2017-10-23 21:58:54 +0100

    Add getMentionedUsers for one tweet only

--- src/twitter/Extract.java
+++ src/twitter/Extract.java
@@ -6,6 +6,7 @@ package twitter;
 import java.util.List;
 import java.time.Instant;
 import java.util.Set;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.lang.StringBuilder;
 
@@ -101,6 +102,25 @@ public class Extract {
         	}
         }
         return unames;
+    }
+    
+    /**
+     * Get usernames mentioned in a tweet tweet.
+     * 
+     * @param tweet
+     *            a tweet not modified by this method.
+     * @return the set of usernames who are mentioned in the text of the tweets.
+     *         A username-mention is "@" followed by a Twitter username (as
+     *         defined by Tweet.getAuthor()'s spec).
+     *         The username-mention cannot be immediately preceded or followed by any
+     *         character valid in a Twitter username.
+     *         For this reason, an email address like bitdiddle@mit.edu does NOT 
+     *         contain a mention of the username mit.
+     *         Twitter usernames are case-insensitive, and the returned set may
+     *         include a username at most once.
+     */
+    public static Set<String> getMentionedUsers(Tweet t) {
+    	return getMentionedUsers(Arrays.asList(t));
     }
     
     /**
